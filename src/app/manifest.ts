a76146ffbd7518bcb2ac5d5df215618959b9f40f
kristinaperez <kristina.perez.28523@gmail.com>
2026-09-18 18:40:06 +0200

    исправляем manifest - any

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,13 +20,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
       {
         src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
   };
